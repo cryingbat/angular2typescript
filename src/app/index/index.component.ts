2f78a5d372cf140a037ef43531a3d8b766fe0c60
cryingbat <haoqi0214@126.com>
2020-03-27 22:43:47 +0800

    add lang variable

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -1,38 +1,58 @@
-import { Component, OnInit } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
+import { Component, OnInit } from "@angular/core";
+import { TranslateService } from "@ngx-translate/core";
 
 export interface Lang {
-  value: string,
-  id: number
+  value: string;
+  id: number;
 }
 @Component({
-  selector: 'app-index',
-  templateUrl: './index.component.html',
-  styleUrls: ['./index.component.css']
+  selector: "app-index",
+  templateUrl: "./index.component.html",
+  styleUrls: ["./index.component.css"]
 })
 export class IndexComponent implements OnInit {
+  obj: object;
+
   constructor(public translate: TranslateService) {
-
+    let curLang = this.translate.currentLang;
+    this._changeProjectName(curLang)
   }
-  ngOnInit() {
+  ngOnInit() {}
+
+  private _changeProjectName (lang: string) {
+    if (lang === "en") {
+      this.obj = {
+        name: "dhq"
+      };
+    } else {
+      this.obj = {
+        name: "浩琦"
+      };
+    }
   }
 
-  languages: Lang[] = [{
-    value: "中文",
-    id: 0 
-  },{
-    value: "English",
-    id: 1
-  }];
+  languages: Lang[] = [
+    {
+      value: "中文",
+      id: 0
+    },
+    {
+      value: "English",
+      id: 1
+    }
+  ];
 
   defaultLang = 0;
-  
+
   langChange(value) {
-    console.log(value)
-    if(value == 0){
-      this.translate.use('zh')
-    }else{
-      this.translate.use('en')
+    let lang: string;
+    if (value == 0) {
+      this.translate.use("zh");
+      lang = 'zh'
+    } else {
+      this.translate.use("en");
+      lang = 'en'
     }
+    this._changeProjectName(lang)
   }
 }
